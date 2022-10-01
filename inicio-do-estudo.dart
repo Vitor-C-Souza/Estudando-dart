d064b738208cad7453839454bee19ebeca88d635
Makinay <vitor_souza97@hotmail.com>
2022-10-01 18:35:42 -0300

    adicionando elementos de orientação de objetos

--- inicio-do-estudo.dart
+++ inicio-do-estudo.dart
@@ -1,58 +1,110 @@
-
+//imports
 import 'dart:io';
 
+//variaveis
+var duration = Duration(seconds: 1);
+const double dinheiro = 2000.24;
+final String nome = "vitor";
+const int tabuada = 48;
+int? resultado;
+List<String> TrioParadaDura = ["Vítor", "Kath", "Lucas"];
+List<dynamic> aleatoriedades = ["Vítor", true, 2];
+bool? falha;
+
 main() {
-  const double dinheiro = 2000.24;
-  final String nome = "vitor";
-  var duration = Duration(seconds: 1);
 
-  const int tabuada = 48;
+  //estanciando classe
+  var tutoriais = new testes();
 
-  int resultado;
-
-  print(nome);
-  print(dinheiro);
-
-  List<String> TrioParadaDura = ["Vítor", "Kath", "Lucas"];
-  List<dynamic> aleatoriedades = ["Vítor", true, 2];
-
-  print(TrioParadaDura);
-
-  print(aleatoriedades);
-
-  print('qual a sua idade:');
+  do{
+    falha = false;
+  // menu
+  print('escolha a sua opção: \n'
+        '1 - entrada de dados \n'
+        '2 - tabuada \n'
+        '3 - exbir Dados \n'
+        '4 - TestandoFor');
   String? input = stdin.readLineSync();
-  
+
   if(input != null){
-    var idade = int.parse(input);
+    int? op = int.parse(input);
 
-  if (idade >= 18){
-    print("maior de idade");
-  } else {
-    print('menor de idade');
+    switch (op){
+      case 1:{
+        tutoriais.EntradaDeDados();
+        break;
+      }
+      case 2:{
+        tutoriais.tabuada(tabuada);
+        break;
+      }
+      case 3:{
+        tutoriais.exbirDados(); 
+        break;
+      }
+      case 4:{
+        tutoriais.TestandoFor();
+        break;
+      }             
+      default:{
+        print('opção inexistente');
+        falha = true;
+      } 
+    }   
   }
-  }
+  }while(falha == true);
+
+  print('fim do programa');
+
+}
+
+
+class testes{
+
+  void EntradaDeDados(){
+    print('qual a sua idade:');
+    String? input = stdin.readLineSync(); //entrada de dados
   
-  for (int i = 1; i <= 5; i++) {
-    print(i);
-  }
-  int j = -1;
-  print('tabuada');
-  do {
-    j++;
+    if(input != null){
+      int idade = int.parse(input);
 
-    resultado = tabuada * j;
-
-    print('$tabuada x $j = $resultado');
-
-    if (j == 10) {
-      break;
+      if (idade >= 18){
+       print("maior de idade");
+      } else {
+      print('menor de idade');
+      } 
     }
+  }
 
-    sleep(duration);
-  } while (true);
 
-  for (int i = 0; i < 10; i++) {
-    print('Os Quase-Vingadores tem $i novos membros!');
+  void tabuada(int tabuada){
+    int count = -1;
+    print('tabuada');
+    do {
+      count++;
+
+      resultado = tabuada * count;
+
+      print('$tabuada x $count = $resultado');
+
+      if (count == 10) {
+        break;
+      }
+
+      sleep(duration);
+    } while (true);
+  }
+
+  void exbirDados(){
+      print(nome + "\n");
+      print( "$dinheiro \n");
+      print("$TrioParadaDura \n");
+      print(aleatoriedades);
+  }
+
+  void TestandoFor(){
+    for (int i = 1; i <= 5; i++) {
+      print(i);
+    }
   }
 }
